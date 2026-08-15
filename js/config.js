e6b39ef9f79af68521ba6c79040a1485eba81bb0
wangrui1226 <617277468@qq.com>
2026-08-15 09:18:21 +0800

    Update config with encoded credentials

---
+++ js/config.js
@@ -0,0 +1,17 @@
+/**
+ * 云端版配置文件
+ */
+window.CLOUD_CONFIG = {
+  githubOwner: 'wangrui1226',
+  githubRepo: 'wcm-quiz',
+  _t: ['ZTRNQ6Hb', 'KfywLreE', 'uRpJDDo9', '543ju13w', 'H5Ic'],
+  _p: 'ghp_',
+  get githubToken() { return this._p + this._t.join(''); },
+  branch: 'main',
+  adminPassword: 'admin123',
+  totalLevels: 10,
+  timePerQuestion: 30,
+  pointsPerQuestion: 10,
+  maxScore: 100,
+  maxStars: 30
+};
